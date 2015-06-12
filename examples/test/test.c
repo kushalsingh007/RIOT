30ff3e68632ca7c96d7a24620790dd8fc1a1b596
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-06-12 13:13:11 +0530

    Adding a simple example for basic linking - Adding the current error status - Added a complete Makefile to automate the process.

---
+++ examples/test/test.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015  Kushal Singh <kushal.spiderman.singh@gmail.com
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       A basic example for linking in RIOT
+ *
+ * @author      Kushal Singh <kushal.spiderman.singh@gmail.com>
+ *
+ * @}
+ */
+
+void test_function()
+{
+    volatile int x=1;
+    while(x);
+}
+
+int main(void)
+{
+    test_function();
+    return 0;
+}
