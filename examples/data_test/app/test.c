13bce0d59ddd5cb34a2711e9492367de6ef0d835
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-07-07 12:58:12 +0530

    Example to show the addition of a variable in .data section -Added updated files. -In order to located x (variable) (Look for deadbeef) -Use readelf/objdump to dig in further and gdb to keep track.

---
+++ examples/data_test/app/test.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015  Kushal Singh <kushal.spiderman.singh@gmail.com
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       A basic example for linking in RIOT
+ *
+ * @author      Kushal Singh <kushal.spiderman.singh@gmail.com>
+ *
+ * @}
+ */
+
+volatile unsigned int a=3735928559;
+volatile unsigned int x=1;
+volatile unsigned int y=3735928559;
+int test(void) {
+    while(x!=1);
+
+    return 1;
+}
