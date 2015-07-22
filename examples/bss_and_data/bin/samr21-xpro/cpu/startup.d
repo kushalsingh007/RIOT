19f3ba850312cc0f9fe723e800c4cfe96e515c70
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-07-22 17:52:20 +0530

    -Adding bin files for testing elf_parser

---
+++ examples/bss_and_data/bin/samr21-xpro/cpu/startup.d
@@ -0,0 +1,17 @@
+/home/kushal/kaspar/RIOT/examples/bss_and_data/bin/samr21-xpro/cpu/startup.o: \
+ /home/kushal/kaspar/RIOT/cpu/samd21/startup.c \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/arm-none-eabi/include/sys/_stdint.h:
---
+++ examples/bss_and_data/bin/samr21-xpro/samr21-xpro_base/board.d
@@ -0,0 +1,253 @@
+/home/kushal/kaspar/RIOT/examples/bss_and_data/bin/samr21-xpro/samr21-xpro_base/board.o: \
+ /home/kushal/kaspar/RIOT/boards/samr21-xpro/board.c \
+ /usr/arm-none-eabi/include/stdio.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/5.1.0/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/5.1.0/include/stdarg.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ /home/kushal/kaspar/RIOT/boards/samr21-xpro/include/board.h \
+ /home/kushal/kaspar/RIOT/cpu/cortexm_common/include/cpu.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/cpu_conf.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/samd21.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cm0plus.h \
+ /home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cmInstr.h \
+ /home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cmFunc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_ac.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_adc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_dmac.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_dsu.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_eic.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_evsys.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_gclk.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_mtb.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_nvmctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_pac.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_pm.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_port.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_rfctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_rtc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_sercom.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_sysctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_tc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_tcc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_usb.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_wdt.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_ac.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_adc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_dmac.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_dsu.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_eic.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_evsys.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_gclk.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_mtb.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_nvmctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac0.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac1.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac2.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pm.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_port.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_rfctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_rtc.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom0.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom1.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom2.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom3.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom4.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom5.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sysctrl.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc3.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc4.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc5.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc0.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc1.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc2.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_usb.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_wdt.h \
+ /home/kushal/kaspar/RIOT/cpu/samd21/include/pio/pio_samr21g18a.h \
+ /home/kushal/kaspar/RIOT/core/include/irq.h \
+ /usr/lib/gcc/arm-none-eabi/5.1.0/include/stdbool.h \
+ /home/kushal/kaspar/RIOT/core/include/arch/irq_arch.h \
+ /home/kushal/kaspar/RIOT/boards/samr21-xpro/include/periph_conf.h
+
+/usr/arm-none-eabi/include/stdio.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/newlib.h:
+
+/usr/arm-none-eabi/include/sys/config.h:
+
+/usr/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/lib/gcc/arm-none-eabi/5.1.0/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/5.1.0/include/stdarg.h:
+
+/usr/arm-none-eabi/include/sys/reent.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/sys/_types.h:
+
+/usr/arm-none-eabi/include/machine/_types.h:
+
+/usr/arm-none-eabi/include/sys/lock.h:
+
+/usr/arm-none-eabi/include/sys/types.h:
+
+/usr/arm-none-eabi/include/sys/_stdint.h:
+
+/usr/arm-none-eabi/include/machine/types.h:
+
+/usr/arm-none-eabi/include/sys/stdio.h:
+
+/home/kushal/kaspar/RIOT/boards/samr21-xpro/include/board.h:
+
+/home/kushal/kaspar/RIOT/cpu/cortexm_common/include/cpu.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/cpu_conf.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/samd21.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
+
+/home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cm0plus.h:
+
+/home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cmInstr.h:
+
+/home/kushal/kaspar/RIOT/cpu/cortexm_common/include/core_cmFunc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_ac.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_adc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_dmac.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_dsu.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_eic.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_evsys.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_gclk.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_mtb.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_nvmctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_pac.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_pm.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_port.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_rfctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_rtc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_sercom.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_sysctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_tc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_tcc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_usb.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/component/component_wdt.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_ac.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_adc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_dmac.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_dsu.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_eic.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_evsys.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_gclk.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_mtb.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_nvmctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac0.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac1.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pac2.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_pm.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_port.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_rfctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_rtc.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom0.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom1.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom2.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom3.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom4.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sercom5.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_sysctrl.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc3.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc4.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tc5.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc0.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc1.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_tcc2.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_usb.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/instance/instance_wdt.h:
+
+/home/kushal/kaspar/RIOT/cpu/samd21/include/pio/pio_samr21g18a.h:
+
+/home/kushal/kaspar/RIOT/core/include/irq.h:
+
+/usr/lib/gcc/arm-none-eabi/5.1.0/include/stdbool.h:
+
+/home/kushal/kaspar/RIOT/core/include/arch/irq_arch.h:
+
+/home/kushal/kaspar/RIOT/boards/samr21-xpro/include/periph_conf.h:
