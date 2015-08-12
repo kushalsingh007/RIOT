a9a22066c953889ccf023ce0a799718a0161094a
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-08-13 02:00:11 +0530

    Abstracted away the pointer arithmetic. - Added functions to remove pointer based direct arithmetic.

--- examples/bss_and_data/riot_elf.h
+++ examples/bss_and_data/riot_elf.h
@@ -3,7 +3,7 @@
 
 #include "elf.h"
 
-void list_shdr_table_entries(char *elf_ptr);
+void list_shdr_table_entries(char *);
 void list_symbol_info(char *);
 void print_symbol_info(Elf32_Half);
 void process_symtab(char *, int);
@@ -11,6 +11,9 @@ void print_rel_type(Elf32_Word);
 void print_sym_name(char *, int, int);
 void process_rel(char *, int );
 void print_break(void);
-Elf32_Addr elf_locate(char * elf_ptr, char *file_ptr);
+Elf32_Addr elf_locate(char *, char *);
+Elf32_Shdr* getElfSectionHdr(char *, int);
+Elf32_Sym* getElfSym(char *, Elf32_Off, int);
+Elf32_Rel* getElfRel(char *, Elf32_Off, int);
 
 #endif /* RIOT_ELF_H */
